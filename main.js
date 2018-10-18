492d98850209f9fbb7566c58a7c138812a73a297
Oluwabammydu <oluwabamise77@gmail.com>
2018-10-18 22:10:58 +0100

    Adding Image gallery file

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-/*var displayedImage = document.querySelector('.displayed-img');
-var thumbBar = document.querySelector('.thumb-bar');
-
-btn = document.querySelector('button');
-var overlay = document.querySelector('.overlay');
-
-/* Looping through images */
-/*
-  var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
-  thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
-
-
-
-
-
-
 var displayedImage = document.querySelector('.displayed-img');
 var thumbBar = document.querySelector('.thumb-bar');
 
